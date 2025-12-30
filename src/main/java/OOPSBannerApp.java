50145ba3c1ecba7697158a488dab8a84e0e0a605
bl-tharun <tarun.prabhu@bridgelabz.com>
2025-12-30 12:26:49 +0530

    UC2: Render OOPS as Banner(spaces and *)

--- src/main/java/OOPSBannerApp.java
+++ src/main/java/OOPSBannerApp.java
@@ -7,6 +7,19 @@
 
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        String[] lines = {
+                "   ***       ***    ******       *****",
+                " **   **   **   **  **    **   **",
+                "**     ** **     ** **     ** **",
+                "**     ** **     ** **    **   **",
+                "**     ** **     ** ******       ***",
+                "**     ** **     ** **              **",
+                "**     ** **     ** **               **",
+                " **   **   **   **  **              **",
+                "   ***       ***    **         *****"
+        };
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
