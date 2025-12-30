ec03a9d1f8627f3545c4dfc89a654a4bd80913af
bl-tharun <tarun.prabhu@bridgelabz.com>
2025-12-30 12:53:27 +0530

    UC3: Refactor Banner Logic into Functions

--- src/main/java/OOPSBannerApp.java
+++ src/main/java/OOPSBannerApp.java
@@ -6,20 +6,61 @@
  */
 
 public class OOPSBannerApp {
+
+    // method to print "O"
+    public static String[] getOPattern() {
+        return new String[]
+                {
+                        "   ***    ",
+                        " **   **  ",
+                        "**     ** ",
+                        "**     ** ",
+                        "**     ** ",
+                        "**     ** ",
+                        "**     ** ",
+                        " **   **  ",
+                        "   ***    "
+                };
+    }
+
+    // method to prin "P"
+    public static String[] getPPattern() {
+        return new String[]
+                {
+                        "******    ",
+                        "**    **  ",
+                        "**     ** ",
+                        "**    **  ",
+                        "******    ",
+                        "**        ",
+                        "**        ",
+                        "**        ",
+                        "**        "
+                };
+    }
+
+    // method to print "S"
+    public static String[] getSPattern() {
+        return new String[]
+                {
+                        "   ***** ",
+                        " **      ",
+                        "**       ",
+                        " **      ",
+                        "   ***   ",
+                        "      ** ",
+                        "       **",
+                        "      ** ",
+                        " *****   "
+                };
+    }
+
     public static void main(String[] args) {
-        String[] lines = {
-                "   ***       ***    ******       *****",
-                " **   **   **   **  **    **   **",
-                "**     ** **     ** **     ** **",
-                "**     ** **     ** **    **   **",
-                "**     ** **     ** ******       ***",
-                "**     ** **     ** **              **",
-                "**     ** **     ** **               **",
-                " **   **   **   **  **              **",
-                "   ***       ***    **         *****"
-        };
-        for (String line : lines) {
-            System.out.println(line);
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+        for(int i = 0; i < oPattern.length; i++){
+            System.out.println(oPattern[i] + oPattern[i] + pPattern[i] + sPattern[i]);
         }
     }
 }
